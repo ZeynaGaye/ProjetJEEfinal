f6d0c15b10ddda638e354ed8da3e810889b54e8c
zeynaGaye <seynabougaye957@gmail.com>
2024-06-25 11:50:41 +0000

    first commit

---
+++ src/main/java/ucad/sn/master2/ProjetJee2024FinalApplication.java
@@ -0,0 +1,13 @@
+package ucad.sn.master2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJee2024FinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetJee2024FinalApplication.class, args);
+    }
+
+}
