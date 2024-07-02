314685936971b2c4e0e700ab1d284d4f6afd07b8
zeynaGaye <seynabougaye957@gmail.com>
2024-07-02 18:24:45 +0000

    ajout du dossier template

--- src/main/java/ucad/sn/master2/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package ucad.sn.master2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ucad.sn.master2.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
