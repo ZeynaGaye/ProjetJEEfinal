15a37a0701cf550291e24d11c4b04683187193aa
Fatima <fatimseck54@gmail.com>
2024-07-06 00:27:46 +0000

    commit

--- src/main/java/ucad/sn/master2/controller/EtudiantController.java
+++ src/main/java/ucad/sn/master2/controller/EtudiantController.java
@@ -1,52 +1,68 @@
 package ucad.sn.master2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ucad.sn.master2.model.Etudiant;
+import ucad.sn.master2.service.ClasseService;
 import ucad.sn.master2.service.EtudiantService;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/etudiants")
 public class EtudiantController {
 
     private final EtudiantService etudiantService;
 
+    private  final  ClasseService classeService;
+
     @Autowired
-    public EtudiantController(EtudiantService etudiantService) {
+    public EtudiantController(EtudiantService etudiantService, ClasseService classeService) {
         this.etudiantService = etudiantService;
+        this.classeService = classeService;
     }
 
     @GetMapping
-    public ResponseEntity<List<Etudiant>> getAllEtudiants() {
+    public String getAllEtudiants(Model model) {
         List<Etudiant> etudiants = etudiantService.getAllEtudiants();
-        return new ResponseEntity<>(etudiants, HttpStatus.OK);
+        model.addAttribute("etudiants", etudiants);
+        return "etudiants/liste";
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Etudiant> getEtudiantById(@PathVariable Long id) {
+    @GetMapping("/add")
+    public String showAddEtudiantForm(Model model) {
+        model.addAttribute("etudiant", new Etudiant());
+        model.addAttribute("classes", classeService.trouverToutesLesClasses());
+        return "etudiants/add";
+    }
+
+    @PostMapping("/save")
+    public String addEtudiant(@ModelAttribute Etudiant etudiant) {
+        etudiantService.saveEtudiant(etudiant);
+        return "redirect:/etudiants";
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable Long id, Model model) {
         Etudiant etudiant = etudiantService.getEtudiantById(id);
-        return new ResponseEntity<>(etudiant, HttpStatus.OK);
+        model.addAttribute("etudiant", etudiant);
+        model.addAttribute("classes", classeService.trouverToutesLesClasses());
+        return "etudiants/edit";
     }
 
-    @PostMapping
-    public ResponseEntity<Etudiant> addEtudiant(@RequestBody Etudiant etudiant) {
-        Etudiant newEtudiant = etudiantService.saveEtudiant(etudiant);
-        return new ResponseEntity<>(newEtudiant, HttpStatus.CREATED);
+
+    @PostMapping("/update/{id}")
+    public String updateEtudiant(@PathVariable Long id, @ModelAttribute Etudiant etudiant) {
+        etudiantService.updateEtudiant(id, etudiant);
+        return "redirect:/etudiants";
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Etudiant> updateEtudiant(@PathVariable Long id, @RequestBody Etudiant etudiant) {
-        Etudiant updatedEtudiant = etudiantService.updateEtudiant(id, etudiant);
-        return new ResponseEntity<>(updatedEtudiant, HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteEtudiant(@PathVariable Long id) {
+    @GetMapping("/delete/{id}")
+    public String deleteEtudiant(@PathVariable Long id) {
         etudiantService.deleteEtudiant(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return "redirect:/etudiants";
     }
 }
